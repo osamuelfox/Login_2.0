0b5b10bf47acba7f66bd238fc8989b9256e83580
osamuelfox <samuelsantosdesouza92@gmail.com>
2024-02-25 21:26:37 -0300

    commit

---
+++ app/src/main/java/com/samuelfox/teste/Tela_Principal.java
@@ -0,0 +1,14 @@
+package com.samuelfox.teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
